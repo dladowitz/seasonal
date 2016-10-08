65ae8fc8b83d81478825a64765fe2b75b4abaec4
David Ladowitz <david.ladowitz@instacart.com>
2016-10-08 10:18:56 -0700

    adding first two states and primary logic

--- lib/seasonal.rb
+++ lib/seasonal.rb
@@ -1,5 +1,30 @@
 require "seasonal/version"
 
-module Seasonal
-  # Your code goes here...
+module Seasons
+  CA = {winter: ["Apple", "Orange", "Avacado"], spring: ["Pear", "Bannana", "Lemon"], summer: ["Cherry", "Watermelon", "Nectarine"], fall: ["Pear", "Bannana", "Lemon"]}.freeze
+  TX = {winter: ["Apple", "Orange", "Avacado"], spring: ["Pear", "Bannana", "Lemon"], summer: ["Cherry", "Orange", "Nectarine"], fall: ["Pear", "Bannana", "Lemon"]}.freeze
+
+  def in_season?(date, state)
+    season = get_season(date)
+    state = string_to_constant(state)
+
+    state[season].include? name
+  end
+
+  def get_season(date)
+    case
+    when date.month <= 3
+      :winter
+    when date.month <= 6
+      :spring
+    when date.month <= 9
+      :summer
+    else
+      :fall
+    end
+  end
+
+  def string_to_constant(string)
+    Seasons.const_get(string)
+  end
 end
